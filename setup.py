b79268e583517d26e5ab960313c0b4d2be26bbe9
Maximilian Nöthe <maximilian.noethe@tu-dortmund.de>
2020-04-30 11:25:08 +0200

    Add pyproject.toml, requirements and more setup config

--- setup.py
+++ setup.py
@@ -1,20 +1,8 @@
 from setuptools import setup, find_packages
 from pyrf import __version__
-import os
 
-def readme():
-    with open("README.md") as f:
-        return f.read()
 
 setup(
-    name="pyrf",
     version=__version__,
-    description="Python IACT IRF builder",
-    url="https://github.com/cta-observatory/pyrf",
-    author="Julien Lefaucheur, Michele Peresano, Thomas Vuillaume",
-    license="MIT",
     packages=find_packages(),
-    package_data={},
-    include_package_data=True,
-    install_requires=[],
 )
