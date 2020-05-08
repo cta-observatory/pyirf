358d93647f95eb947acd0f4b09897ee25172bfb6
vuillaut <thomas.vuillaume@gmail.com>
2020-05-09 00:21:40 +0200

    perf module

---
+++ pyrf/perf/__init__.py
@@ -0,0 +1,12 @@
+from .irf_maker import IrfMaker, SensitivityMaker, BkgData, Irf
+from .cut_optimisation import CutsOptimisation, CutsDiagnostic, CutsApplicator
+
+__all__ = [
+    'IrfMaker',
+    'SensitivityMaker',
+    'BkgData',
+    'Irf',
+    'CutsOptimisation',
+    'CutsDiagnostic',
+    'CutsApplicator',
+]
