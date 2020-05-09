bdc9277815c0ab11efef1a97739f2340242054bd
vuillaut <thomas.vuillaume@gmail.com>
2020-05-09 02:10:14 +0200

    add install requires

--- setup.py
+++ setup.py
@@ -5,4 +5,15 @@ exec(open('pyrf/version.py').read())
 setup(
     version=__version__,
     packages=find_packages(),
+    install_requires=[
+        'astropy',
+        'ctaplot~=0.5.0',
+        'gammapy=0.8',
+        'matplotlib',
+        'numpy',
+        'pandas',
+        'scipy',
+        'scikit-learn',
+        'tables',
+    ],
 )
