77c430ebde299ed91b6f268fbf5b1af19b23c171
Michele Peresano <michele.peresano@cea.fr>
2020-09-04 14:44:25 +0200

    add config file as package data

--- setup.py
+++ setup.py
@@ -1,19 +1,21 @@
 from setuptools import setup, find_packages
 
-exec(open('pyirf/version.py').read())
+exec(open("pyirf/version.py").read())
 
 setup(
     version=__version__,
     packages=find_packages(),
+    package_data={"pyirf": ["resources/config.yml"]},
+    include_package_data=True,
     install_requires=[
-        'astropy',
-        'ctaplot~=0.5.0',
-        'gammapy==0.8',
-        'matplotlib',
-        'numpy',
-        'pandas',
-        'scipy',
-        'tables',
-        'ctapipe==0.7',
+        "astropy",
+        "ctaplot~=0.5.0",
+        "gammapy==0.8",
+        "matplotlib",
+        "numpy",
+        "pandas",
+        "scipy",
+        "tables",
+        "ctapipe==0.7",
     ],
 )
