21ad595bc6124de0611d42fd365dbf786e5cd7a7
Maximilian Nöthe <maximilian.noethe@tu-dortmund.de>
2020-09-21 11:12:03 +0200

    Add test requirements

--- setup.py
+++ setup.py
@@ -4,6 +4,22 @@ import re
 with open("pyirf/version.py") as f:
     __version__ = re.search('^__version__ = "(.*)"$', f.read()).group(1)
 
+extras_require = {
+    'docs': [
+        'rinohtype',
+        'sphinx',
+        'sphinx_rtd_theme',
+        'sphinx_automodapi',
+        'numpydoc',
+        'nbsphinx'
+    ],
+    'tests': [
+        'pytest',
+    ],
+}
+
+extras_require['all'] = extras_require['tests'] + extras_require['docs']
+
 setup(
     version=__version__,
     packages=find_packages(),
@@ -17,14 +33,5 @@ setup(
         "tables",
         "gammapy~=0.8.0",
     ],
-    extras_require={
-        'docs': [
-            'rinohtype',
-            'sphinx',
-            'sphinx_rtd_theme',
-            'sphinx_automodapi',
-            'numpydoc',
-            'nbsphinx'
-        ]
-    }
+    extras_require=extras_require,
 )
