1b9bfd7e59a428001de3a3bd02b24b6780a89ed2
Maximilian Nöthe <maximilian.noethe@tu-dortmund.de>
2020-09-29 13:15:48 +0200

    Calculate effective area in bins of energy and fov offset

--- pyirf/irf/__init__.py
+++ pyirf/irf/__init__.py
@@ -1,11 +1,16 @@
-from .effective_area import effective_area, point_like_effective_area
+from .effective_area import (
+    effective_area,
+    effective_area_energy_fov,
+    effective_area_energy,
+)
 from .energy_dispersion import energy_dispersion
 from .psf import psf_table
 from .background import background_2d
 
 __all__ = [
     "effective_area",
-    "point_like_effective_area",
+    "effective_area_energy",
+    "effective_area_energy_fov",
     "energy_dispersion",
     "psf_table",
     "background_2d",
