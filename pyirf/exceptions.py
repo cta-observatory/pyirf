4970a21fc30626b0ff9dbdd3209f36f284354c7a
Maximilian Nöthe <maximilian.noethe@tu-dortmund.de>
2020-09-30 13:56:37 +0200

    Add docstrings

--- pyirf/exceptions.py
+++ pyirf/exceptions.py
@@ -3,11 +3,15 @@ class IRFException(Exception):
 
 
 class MissingColumns(IRFException):
+    ''' Exception to be raised when a table is missing a required column'''
+
     def __init__(self, columns):
         super().__init__(f'Table is missing required columns {columns}')
 
 
 class WrongColumnUnit(IRFException):
+    ''' Exception to be raised when a column of a table has the wrong unit'''
+
     def __init__(self, column, unit, expected):
         super().__init__(
             f'Unit {unit} of column "{column}"'
