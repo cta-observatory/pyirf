f5d617d847712b2d54ab86534941d6a27d39e303
Michele Peresano <michele.peresano@cea.fr>
2020-10-02 14:40:05 +0200

    Improvements to path handling and logging.

--- pyirf/tests/test_integration.py
+++ pyirf/tests/test_integration.py
@@ -1,50 +1,50 @@
-import glob
 import subprocess
 import logging
 import os
+import sys
 from astropy.io import fits
 from ogadf_schema.irfs import AEFF_2D, EDISP_2D, PSF_TABLE, BKG_2D, RAD_MAX
+from pathlib import Path
 
 
-def test_integration_test(caplog):
+def test_eventdisplay_example(caplog):
+    """Runs the EventDisplay example script and check its output."""
 
-    caplog.set_level(logging.INFO)
+    caplog.set_level(logging.WARNING, logger="fits_schema")
+    ROOT_DIR = Path(__file__).parent.parent.parent
 
-    ROOT_DIR = os.path.dirname(os.path.abspath("setup.py"))
+    # run script and check that it doesn't crash
+    script_path = os.path.join(ROOT_DIR, "examples/calculate_eventdisplay_irfs.py")
+    subprocess.check_output([sys.executable, script_path])
 
-    for example_script in glob.glob(f"{ROOT_DIR}/examples/calculate_*.py"):
+    # check that the output file exists and it's not empty
+    outname = os.path.join(ROOT_DIR, "pyirf_eventdisplay.fits.gz")
+    assert os.path.isfile(outname) and os.path.getsize(outname)
 
-        # run script and check that it doesn't crash
-        subprocess.check_output(f"python {example_script}", shell=True)
+    # open FITS file
+    outfile = fits.open(outname)
 
-        # check that the output file exists and it's not empty
-        outname = f"{ROOT_DIR}/pyirf_eventdisplay.fits.gz"
-        outfile = fits.open(outname)
+    # known errors
+    dimensionality_error = "Dimensionality of rows is 0, should be 1"
 
-        assert os.path.isfile(outname) and os.path.getsize(outname)
-
-        # check that the output file respects the OGADF schema
-
-        # known errors
-        dimensionality_error = "Dimensionality of rows is 0, should be 1"
-
-        AEFF_2D.validate_hdu(outfile["EFFECTIVE_AREA"], onerror="log")
-        assert str([rec.message for rec in caplog.records]) == str(
-            [dimensionality_error] * 2
-        )
-        EDISP_2D.validate_hdu(outfile["ENERGY_DISPERSION"], onerror="log")
-        assert str([rec.message for rec in caplog.records]) == str(
-            [dimensionality_error] * 4
-        )
-        PSF_TABLE.validate_hdu(outfile["PSF"], onerror="log")
-        assert str([rec.message for rec in caplog.records]) == str(
-            [dimensionality_error] * 6
-        )
-        BKG_2D.validate_hdu(outfile["BACKGROUND"], onerror="log")
-        assert str([rec.message for rec in caplog.records]) == str(
-            [dimensionality_error] * 6
-        )
-        RAD_MAX.validate_hdu(outfile["RAD_MAX"], onerror="log")
-        assert str([rec.message for rec in caplog.records]) == str(
-            [dimensionality_error] * 8
-        )
+    # check that each HDU respects the OGADF schema
+    AEFF_2D.validate_hdu(outfile["EFFECTIVE_AREA"], onerror="log")
+    assert str([rec.message for rec in caplog.records]) == str(
+        [dimensionality_error] * 2
+    )
+    EDISP_2D.validate_hdu(outfile["ENERGY_DISPERSION"], onerror="log")
+    assert str([rec.message for rec in caplog.records]) == str(
+        [dimensionality_error] * 4
+    )
+    PSF_TABLE.validate_hdu(outfile["PSF"], onerror="log")
+    assert str([rec.message for rec in caplog.records]) == str(
+        [dimensionality_error] * 6
+    )
+    BKG_2D.validate_hdu(outfile["BACKGROUND"], onerror="log")
+    assert str([rec.message for rec in caplog.records]) == str(
+        [dimensionality_error] * 6
+    )
+    RAD_MAX.validate_hdu(outfile["RAD_MAX"], onerror="log")
+    assert str([rec.message for rec in caplog.records]) == str(
+        [dimensionality_error] * 8
+    )
