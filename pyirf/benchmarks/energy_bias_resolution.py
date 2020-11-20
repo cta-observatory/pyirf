62fd7b1d9151b542ba455f5934d1fea1b811ea5b
Michele Peresano <michele.peresano@cea.fr>
2020-11-20 16:23:05 +0100

    Do the same for energy resolution and bias

--- pyirf/benchmarks/energy_bias_resolution.py
+++ pyirf/benchmarks/energy_bias_resolution.py
@@ -52,7 +52,8 @@ def inter_quantile_distance(rel_error):
 
 def energy_bias_resolution(
     events,
-    true_energy_bins,
+    energy_bins,
+    energy_type="true",
     bias_function=np.median,
     resolution_function=inter_quantile_distance,
 ):
@@ -63,8 +64,11 @@ def energy_bias_resolution(
     ----------
     events: astropy.table.QTable
         Astropy Table object containing the reconstructed events information.
-    true_energy_bins: numpy.ndarray(dtype=float, ndim=1)
-        Bin edges in true energy.
+    energy_bins: numpy.ndarray(dtype=float, ndim=1)
+        Bin edges in energy.
+    energy_type: str
+        Either "true" or "reco" energy.
+        Default is "true".
     bias_function: callable
         Function used to calculate the energy bias
     resolution_function: callable
@@ -82,13 +86,13 @@ def energy_bias_resolution(
     table["rel_error"] = (events["reco_energy"] / events["true_energy"]) - 1
 
     table["bin_index"] = calculate_bin_indices(
-        table["true_energy"].quantity, true_energy_bins
+        table[f"{energy_type}_energy"].quantity, energy_bins
     )
 
     result = Table()
-    result["true_energy_low"] = true_energy_bins[:-1]
-    result["true_energy_high"] = true_energy_bins[1:]
-    result["true_energy_center"] = 0.5 * (true_energy_bins[:-1] + true_energy_bins[1:])
+    result[f"{energy_type}_energy_low"] = energy_bins[:-1]
+    result[f"{energy_type}_energy_high"] = energy_bins[1:]
+    result[f"{energy_type}_energy_center"] = 0.5 * (energy_bins[:-1] + energy_bins[1:])
 
     result["bias"] = np.nan
     result["resolution"] = np.nan
