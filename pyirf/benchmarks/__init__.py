6d9da0ed7c21f2ee07c1ee86f23f5d27326eedc2
Maximilian Nöthe <maximilian.noethe@tu-dortmund.de>
2021-03-23 20:42:31 +0100

    Add function to compute bias and resolution from energy dispersion

--- pyirf/benchmarks/__init__.py
+++ pyirf/benchmarks/__init__.py
@@ -1,8 +1,9 @@
-from .energy_bias_resolution import energy_bias_resolution
+from .energy_bias_resolution import energy_bias_resolution, energy_bias_resolution_from_energy_dispersion
 from .angular_resolution import angular_resolution
 
 
 __all__ = [
     "energy_bias_resolution",
+    "energy_bias_resolution_from_energy_dispersion",
     "angular_resolution",
 ]
