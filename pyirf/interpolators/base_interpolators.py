4399253d4ca069b1c1d413a2efbab8b8901d3723
Rune Michael Dominik <rune.dominik@tu-dortmund.de>
2023-01-24 16:10:42 +0100

    Add base interpolators

---
+++ pyirf/interpolators/base_interpolators.py
@@ -0,0 +1,198 @@
+"""Base classes for interpolators"""
+import numpy as np
+from pyirf.binning import bin_center
+from scipy.spatial import Delaunay, QhullError
+
+
+class BaseInterpolator:
+    """
+    Base class for all interpolators, only knowing grid-points,
+    providing a common __call__-interface and doing sanity checks.
+    """
+
+    def __init__(self, grid_points):
+        """BaseInterpolator constructor, doing sanity checks and ensuring
+        correct shapes of inputs
+
+        Args:
+            grid_points (np.ndarray, shape=[n_points, n_dims]): Grid points at which
+                interpolation templates exist
+
+        Raises:
+            TypeError: When grid_points is not a np.ndarray
+            TypeError: When grid_point has dtype object
+            ValueError: When there are too few points in grid_points to span a volume
+                in the grid dimension.
+        """
+        if not isinstance(grid_points, np.ndarray):
+            raise TypeError("Input grid_points is not a numpy array.")
+        if grid_points.dtype == "O":
+            raise TypeError("Input grid_points array cannot be of dtype object.")
+
+        self.grid_points = grid_points
+        if self.grid_points.ndim == 1:
+            self.grid_points = self.grid_points.reshape(*self.grid_points.shape, 1)
+        self.n_points = self.grid_points.shape[0]
+        self.grid_dim = self.grid_points.shape[1]
+
+        # Check, if number of grid point theoretically suffices to span a volume
+        # in the dimension indicated by grid
+        if self.n_points < self.grid_dim + 1:
+            raise ValueError(
+                f"To few points for grid dimension, grid-dim is {self.grid_dim},"
+                f" while there are only {self.n_points}. At least {self.grid_dim+1}"
+                f" points needed to span a volume in {self.grid_dim} dimensions."
+            )
+
+        # Build triangulation to check if target is inside of the grid for
+        # more then 1 dimension
+        if self.grid_dim > 1:
+            try:
+                self.Triangulation = Delaunay(self.grid_points)
+            except QhullError:
+                raise
+
+    def _interpolate(self, target_point, **kwargs):
+        """Overridable function for the actual interpolation code"""
+        raise NotImplementedError
+
+    def _target_in_grid(self, target_point):
+        """Check wether target_point lies within grids convex hull"""
+        if self.grid_dim == 1:
+            return (target_point >= self.grid_points.min()) and (
+                target_point <= self.grid_points.max()
+            )
+        else:
+            # Delaunay.find_simplex() returns -1 for points outside the grids convex hull
+            simplex_ind = self.Triangulation.find_simplex(target_point)
+            return simplex_ind >= 0
+
+    def __call__(self, target_point, extrapolator=None, **kwargs):
+        """Providing a common __call__ interface sanity checking the target point
+
+        Args:
+            target_point (np.ndarray): Target for inter-/extrapolation
+            extrapolator (callable, optional): Fall-Back extrapolator called when target_point is outside grid.
+                Defaults to None, meaning no extrapolation is done.
+
+        Raises:
+            TypeError: When target_point is not an np.ndarray
+            ValueError: When more then one target_point is given
+            ValueError: When target_point and grid_points have miss-matching dimensions
+            ValueError: When target_point is outside of the grids convex hull but extrapolator is None
+
+        Returns:
+            Interpolated or, if necessary extrapolated, result.
+        """
+        if not isinstance(target_point, np.ndarray):
+            raise TypeError("Target point is not a numpy array.")
+
+        if target_point.ndim == 1:
+            target_point = target_point.reshape(1, *target_point.shape)
+        elif target_point.shape[0] != 1:
+            raise ValueError("Only one target_point per call supported.")
+
+        if target_point.shape[1] != self.grid_dim:
+            raise ValueError(
+                "Missmatch between target-point and grid dimension."
+                f" Grid has dimension {self.grid_dim}, target has dimension"
+                f" {target_point.shape[1]}."
+            )
+
+        if self._target_in_grid(target_point):
+            return self._interpolate(target_point, **kwargs)
+        elif extrapolator is not None:
+            print(f"Trying to extrapolate for point {target_point}.")
+            try:
+                return extrapolator(target_point, **kwargs)
+            except:
+                raise
+        else:
+            raise ValueError(
+                "Target point outside grids convex hull and no extrapolator given."
+            )
+
+
+class ParametrizedInterpolator(BaseInterpolator):
+    """
+    Base class for all interpolators used with parametrized IRF components like 3Gauss,
+    extending BaseInterpolators sanity checks.
+    Derived from pyirf.interpolators.BaseInterpolator
+    """
+
+    def __init__(self, grid_points, params):
+        """ParametrizedInterpolator constructor
+
+        Args:
+            grid_points (np.ndarray): Grid points at which interpolation templates exist
+            params (np.ndarray): Corresponding parameter values at each point
+                in grid_points
+
+        Raises:
+            TypeError: When params is not a np.ndarray
+            ValueError: When number of points grid_points and params is not matching
+
+        Note:
+            Also calls pyirf.interpolators.BaseInterpolators.__call__
+        """
+        super().__init__(grid_points)
+
+        if not isinstance(params, np.ndarray):
+            raise TypeError("Input params is not a numpy array.")
+        elif self.n_points != params.shape[0]:
+            raise ValueError(
+                "Shape missmatch, number of grid_points and rows in params not matching."
+            )
+        else:
+            self.params = params
+
+        if self.params.ndim == 1:
+            self.params = self.params.reshape(*self.params.shape, 1)
+
+
+class BinnedInterpolator(BaseInterpolator):
+    """
+    Base class for all interpolators used with binned IRF components like EDisp,
+    extending BaseInterpolators sanity checks.
+    Derived from pyirf.interpolators.BaseInterpolator
+    """
+
+    def __init__(self, grid_points, bin_edges, bin_contents):
+        """BinnedInterpolator constructor
+
+        Args:
+            grid_points (np.ndarray): Grid points at which interpolation templates exist
+            bin_edges (np.ndarray): Edges of the data binning
+            bin_content (np.ndarray): Content of each bin in bin_edges for
+                each point in grid_points
+
+        Raises:
+            TypeError: When bin_edges is not a np.ndarray
+            TypeError: When bin_content is not a np.ndarray
+            ValueError: When number of bins in bin_edges and contents in
+                bin_contents is not matching
+            ValueError: When number of histograms in bin_contents and
+                points in grid_points is not matching
+
+        Note:
+            Also calls pyirf.interpolators.BaseInterpolators.__call__
+        """
+        super().__init__(grid_points)
+
+        if not isinstance(bin_edges, np.ndarray):
+            raise TypeError("Input bin_edges is not a numpy array.")
+        elif not isinstance(bin_contents, np.ndarray):
+            raise TypeError("Input bin_contents is not a numpy array.")
+        elif bin_contents.shape[-1] != (bin_edges.shape[0] - 1):
+            raise ValueError(
+                "Shape missmatch, bin_edges and bin_contents not matching."
+            )
+        elif self.n_points != bin_contents.shape[0]:
+            raise ValueError(
+                "Shape missmatch, number of grid_points and histograms in bin_contents"
+                " not matching."
+            )
+        else:
+            self.bin_edges = bin_edges
+            self.bin_mids = bin_center(self.bin_edges)
+            self.bin_contents = bin_contents
---
+++ pyirf/interpolators/tests/test_base_interpolators.py
@@ -0,0 +1,151 @@
+"""Tests for base interpolator classes"""
+import pytest
+import numpy as np
+
+
+def test_BaseInterpolator_datastructure_checks():
+    """Test checks performed to enforce correct data structure"""
+    from pyirf.interpolators.base_interpolators import BaseInterpolator
+
+    grid_points1D_good = np.array([1, 2, 3])
+    grid_points1D_bad = [1, 2, 3]
+    target1D_bad = 1
+
+    grid_points2D_good = np.array([[1, 1], [2, 1], [1.5, 1.5]])
+    grid_points2D_bad = np.array([[1, 1], [2, 1], [1.5]], dtype="object")
+    target2D_bad = [1.25, 1.25]
+
+    with pytest.raises(TypeError):  # Grid is a list
+        BaseInterpolator(grid_points1D_bad)
+
+    with pytest.raises(TypeError):  # Grid is an array with dtype object
+        BaseInterpolator(grid_points2D_bad)
+
+    with pytest.raises(TypeError):  # Target is an integer
+        Interp = BaseInterpolator(grid_points1D_good)
+        Interp(target1D_bad)
+
+    with pytest.raises(TypeError):  # Target is a list
+        Interp = BaseInterpolator(grid_points2D_good)
+        Interp(target2D_bad)
+
+
+def test_BaseInterpolator_sanity_checks():
+    """Test checks performed to enforce correct data values"""
+    from pyirf.interpolators.base_interpolators import BaseInterpolator
+
+    grid_points1D_good = np.array([1, 2, 3])
+    target1D_inGrid = np.array([1.5])
+    target1D_outofGrid = np.array([0.5])
+
+    grid_points2D_good = np.array([[1, 1], [2, 1], [1.5, 1.5]])
+    grid_points2D_toofew = np.array([[1, 1], [2, 1]])
+    target2D_inGrid = np.array([1.25, 1.25])
+    target2D_outofGrid = np.array([0.5, 0.5])
+    target2D_twopoints = np.array([[1.25, 1.25], [1.25, 1.25]])
+
+    with pytest.raises(ValueError):  # Target out of grid
+        interp = BaseInterpolator(grid_points1D_good)
+        interp(target1D_outofGrid)
+
+    with pytest.raises(ValueError):  # To few grid points
+        BaseInterpolator(grid_points2D_toofew)
+
+    with pytest.raises(ValueError):  # Two target points
+        interp = BaseInterpolator(grid_points2D_good)
+        interp(target2D_twopoints)
+
+    with pytest.raises(ValueError):  # Target out of grid
+        interp = BaseInterpolator(grid_points2D_good)
+        interp(target2D_outofGrid)
+
+    with pytest.raises(ValueError):  # 1D target in 2D grid
+        interp = BaseInterpolator(grid_points2D_good)
+        interp(target1D_inGrid)
+
+    with pytest.raises(NotImplementedError):
+        # Everything ok but _interpolate not implemented
+        interp = BaseInterpolator(grid_points1D_good)
+        interp(target1D_inGrid)
+
+    with pytest.raises(NotImplementedError):
+        # Everything ok but _interpolate not implemented
+        interp = BaseInterpolator(grid_points2D_good)
+        interp(target2D_inGrid)
+
+
+def test_BaseInterpolator_extrapolation():
+    """Test extrapolators are correctly passed"""
+    from pyirf.interpolators.base_interpolators import BaseInterpolator
+
+    def dummy_extrapolator(target_point):
+        return np.sum(target_point)
+
+    grid_points1D_good = np.array([1, 2, 3])
+    target1D_outofGrid = np.array([0.5])
+
+    grid_points2D_good = np.array([[1, 1], [2, 1], [1.5, 1.5]])
+    target2D_outofGrid = np.array([0.5, 0.5])
+
+    # 1D target out of grid but extrapolator given
+    interp1D = BaseInterpolator(grid_points1D_good)
+    assert interp1D(target1D_outofGrid, extrapolator=dummy_extrapolator) == np.sum(
+        target1D_outofGrid
+    )
+
+    # 2D target out of grid but extrapolator given
+    interp2D = BaseInterpolator(grid_points2D_good)
+    assert interp2D(target2D_outofGrid, extrapolator=dummy_extrapolator) == np.sum(
+        target2D_outofGrid
+    )
+
+
+def test_ParametrizedInterpolator():
+    """Test ParametrizedInterpolator initialization and sanity checks"""
+    from pyirf.interpolators.base_interpolators import ParametrizedInterpolator
+
+    grid_points = np.array([1, 2, 3])
+    params_good = np.array([[1], [2], [3]])
+    params_shape_missmatch = np.array([[1], [2]])
+
+    with pytest.raises(TypeError):  # parameters not a np.ndarray
+        ParametrizedInterpolator(grid_points, params_good.tolist())
+
+    with pytest.raises(ValueError):  # Fewer parameters then grid_points
+        ParametrizedInterpolator(grid_points, params_shape_missmatch)
+
+    interp = ParametrizedInterpolator(grid_points, params_good)
+
+    with pytest.raises(NotImplementedError):
+        # Everything ok but _interpolate not implemented
+        interp(np.array([1]))
+
+
+def test_BinnedInterpolator():
+    """Test BinnedInterpolator initialization and sanity checks"""
+    from pyirf.interpolators.base_interpolators import BinnedInterpolator
+
+    grid_points = np.array([1, 2, 3])
+    bin_edges = np.linspace(-1, 1, 11)
+    bin_content_good_shape = np.ones(shape=(len(grid_points), len(bin_edges) - 1))
+    bin_content_bad_nhist = np.ones(shape=(len(grid_points) - 1, len(bin_edges) - 1))
+    bin_content_bad_nbins = np.ones(shape=(len(grid_points), len(bin_edges)))
+
+    with pytest.raises(TypeError):  # bin_edges are not a np.ndarray
+        BinnedInterpolator(grid_points, bin_edges.tolist(), bin_content_good_shape)
+
+    with pytest.raises(TypeError):  # bin_contents are not a np.ndarray
+        BinnedInterpolator(grid_points, bin_edges, bin_content_good_shape.tolist())
+
+    with pytest.raises(ValueError):  # Fewer entries in bin_contents then grid_points
+        BinnedInterpolator(grid_points, bin_edges, bin_content_bad_nhist)
+
+    with pytest.raises(ValueError):
+        # Fewer entries per bin-content then indicated by bin_edges
+        BinnedInterpolator(grid_points, bin_edges, bin_content_bad_nbins)
+
+    interp = BinnedInterpolator(grid_points, bin_edges, bin_content_good_shape)
+
+    with pytest.raises(NotImplementedError):
+        # Everything ok but _interpolate not implemented
+        interp(np.array([1]))
