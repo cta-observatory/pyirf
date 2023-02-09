b6484c3f845cf242994e7bb004a98421a80ee7a8
Rune Dominik <rune.dominik@tu-dortmund.de>
2023-02-09 15:37:30 +0100

    Forcefully cast to float64

--- pyirf/statistics.py
+++ pyirf/statistics.py
@@ -2,7 +2,7 @@ import numpy as np
 
 from .utils import is_scalar
 
-__all__  = ['li_ma_significance']
+__all__ = ["li_ma_significance"]
 
 
 def li_ma_significance(n_on, n_off, alpha=0.2):
@@ -32,8 +32,11 @@ def li_ma_significance(n_on, n_off, alpha=0.2):
 
     scalar = is_scalar(n_on)
 
-    n_on = np.array(n_on, copy=False, ndmin=1)
-    n_off = np.array(n_off, copy=False, ndmin=1)
+    # Cast everything into float64 to avoid numeric instabilties
+    # when multiplying very small and very big numbers to get t1 and t2
+    n_on = np.array(n_on, copy=False, ndmin=1, dtype=np.float64)
+    n_off = np.array(n_off, copy=False, ndmin=1, dtype=np.float64)
+    alpha = np.float64(alpha)
 
     with np.errstate(divide="ignore", invalid="ignore"):
         p_on = n_on / (n_on + n_off)
