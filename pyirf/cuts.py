fe0b5b26185f29a3edd8862213ef6899284b8df3
gschwefer <georg.schwefer@mpi-hd.mpg.de>
2023-04-19 14:49:07 +0200

    Added weighted percentile cut

--- pyirf/cuts.py
+++ pyirf/cuts.py
@@ -6,15 +6,58 @@ import astropy.units as u
 from .binning import calculate_bin_indices, bin_center
 
 __all__ = [
-    'calculate_percentile_cut',
-    'evaluate_binned_cut',
-    'compare_irf_cuts',
+    "calculate_percentile_cut",
+    "evaluate_binned_cut",
+    "compare_irf_cuts",
+    "weighted_quantile",
 ]
 
 
+def _weighted_quantile_arg(values, weights, q=0.5):
+    """Helper function for weighted quantile
+    Returns:
+    index of the weighted quantile
+    """
+    values = values[~np.isnan(values)]
+    weights = weights[~np.isnan(values)]
+    indices = np.argsort(values)
+    sorted_indices = np.arange(len(values))[indices]
+    medianidx = (weights[indices].cumsum() / weights[indices].sum()).searchsorted(q)
+    if (0 <= medianidx) and (medianidx < len(values)):
+        return sorted_indices[medianidx]
+    else:
+        return np.nan
+
+
+def weighted_quantile(values, weights, q=0.5):
+    """Calculate weighted quantile
+    Args:
+    values , data array
+    weights, weight array
+    q, Quantile from (0,1)
+    Returns:
+    weighted quantile
+    """
+    if len(values) != len(weights):
+        raise ValueError("shape of values and weights doesn't match!")
+    index = _weighted_quantile_arg(values, weights, q=q)
+    if index != np.nan:
+        return values[index]
+    else:
+        return np.nan
+
+
 def calculate_percentile_cut(
-    values, bin_values, bins, fill_value, percentile=68, min_value=None, max_value=None,
-    smoothing=None, min_events=10,
+    values,
+    bin_values,
+    bins,
+    fill_value,
+    weights=None,
+    percentile=68,
+    min_value=None,
+    max_value=None,
+    smoothing=None,
+    min_events=10,
 ):
     """
     Calculate cuts as the percentile of a given quantity in bins of another
@@ -31,6 +74,10 @@ def calculate_percentile_cut(
     fill_value: float or quantity
         Value for bins with less than ``min_events``,
         must have same unit as values
+    weights: ``~numpy.ndarray``
+        Array containing the weight of each entry in ```values```.
+        The default value of None corresponds to equal weights.
+        Must be of same length as ```values```.
     percentile: float
         The percentile to calculate in each bin as a percentage,
         i.e. 0 <= percentile <= 100.
@@ -44,9 +91,14 @@ def calculate_percentile_cut(
     min_events: int
         Bins with less events than this number are replaced with ``fill_value``
     """
+
+    if weights is None:
+        weights = np.ones(len(values))
+    else:
+        assert len(weights) == len(values)
     # create a table to make use of groupby operations
     # we use a normal table here to avoid astropy/astropy#13840
-    table = Table({"values": values}, copy=False)
+    table = Table({"values": values, "weights": weights}, copy=False)
     unit = table["values"].unit
 
     # make sure units match
@@ -77,17 +129,19 @@ def calculate_percentile_cut(
         if n_events < min_events:
             cut_table["cut"][bin_idx] = fill_value
         else:
-            value = np.nanpercentile(group["values"], percentile)
+            value = weighted_quantile(
+                group["values"], group["weights"], percentile / 100
+            )
             if min_value is not None or max_value is not None:
                 value = np.clip(value, min_value, max_value)
 
             cut_table["cut"].value[bin_idx] = value
 
     if smoothing is not None:
-        cut_table['cut'].value[:] = gaussian_filter1d(
+        cut_table["cut"].value[:] = gaussian_filter1d(
             cut_table["cut"].value,
             smoothing,
-            mode='nearest',
+            mode="nearest",
         )
 
     return cut_table
@@ -119,7 +173,7 @@ def evaluate_binned_cut(values, bin_values, cut_table, op):
         Must support vectorized application.
     """
     if not isinstance(cut_table, QTable):
-        raise ValueError('cut_table needs to be an astropy.table.QTable')
+        raise ValueError("cut_table needs to be an astropy.table.QTable")
 
     bins = np.append(cut_table["low"], cut_table["high"][-1])
     bin_index, valid = calculate_bin_indices(bin_values, bins)
