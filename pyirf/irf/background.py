7c6d15848131e29ea7405596e5ef29481d00b151
Tomas Bylund <tomas.bylund@cea.fr>
2024-02-06 15:26:27 +0100

    Should now support rectangular arrays

--- pyirf/irf/background.py
+++ pyirf/irf/background.py
@@ -4,7 +4,7 @@ import numpy as np
 from ..utils import cone_solid_angle
 
 #: Unit of the background rate IRF
-BACKGROUND_UNIT = u.Unit('s-1 TeV-1 sr-1')
+BACKGROUND_UNIT = u.Unit("s-1 TeV-1 sr-1")
 
 
 def background_2d(events, reco_energy_bins, fov_offset_bins, t_obs):
@@ -43,7 +43,7 @@ def background_2d(events, reco_energy_bins, fov_offset_bins, t_obs):
             reco_energy_bins.to_value(u.TeV),
             fov_offset_bins.to_value(u.deg),
         ],
-        weights=events['weight'],
+        weights=events["weight"],
     )
 
     # divide all energy bins by their width
@@ -74,7 +74,7 @@ def background_3d(events, reco_energy_bins, fov_offset_bins, t_obs):
     reco_energy: astropy.units.Quantity[energy]
         The bins in reconstructed energy to be used for the IRF
     fov_offset_bins: astropy.units.Quantity[angle]
-        The bins in the field of view offset to be used for the IRF
+        The bins in the field of view offset to be used for the IRF, either a (N,) or (1,N) or a (2,N) array
     t_obs: astropy.units.Quantity[time]
         Observation time. This must match with how the individual event
         weights are calculated.
@@ -87,6 +87,17 @@ def background_3d(events, reco_energy_bins, fov_offset_bins, t_obs):
 
         Shape: (len(reco_energy_bins) - 1,  len(fov_offset_bins) - 1, len(fov_offset_bins) - 1)
     """
+    if (fov_offset_bins.shape[0] == 1) or (len(fov_offset_bins.shape) == 1):
+        fov_x_offset_bins = fov_offset_bins
+        fov_y_offset_bins = fov_offset_bins
+    elif fov_offset_bins.shape[0] == 2:
+        fov_x_offset_bins = fov_offset_bins[0, :]
+        fov_y_offset_bins = fov_offset_bins[1, :]
+    else:
+        raise ValueError(
+            f"fov_offset_bins must be eiher (N,) or (1,N) or (2,N), found {fov_offset_bins.shape}"
+        )
+
     hist, _ = np.histogramdd(
         [
             events["reco_energy"].to_value(u.TeV),
@@ -95,8 +106,8 @@ def background_3d(events, reco_energy_bins, fov_offset_bins, t_obs):
         ],
         bins=[
             reco_energy_bins.to_value(u.TeV),
-            fov_offset_bins.to_value(u.deg),
-            fov_offset_bins.to_value(u.deg),
+            fov_x_offset_bins.to_value(u.deg),
+            fov_y_offset_bins.to_value(u.deg),
         ],
         weights=events["weight"],
     )
