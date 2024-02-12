14b050ee303e46c3d9df950c0e8ff5cdbb4ab40e
Luca Di Bella <ldibella@Janas-Mac-mini.e5.physik.tu-dortmund.de>
2024-02-12 17:35:58 +0100

    Add 2 methods to calculate effective area in energy and 2 spacial dimensions
    The different methods use binning in different coordinate systems
     - effective_area_3D_polar uses a polar coordinate system with radial FOV offset and azimuthal FOV position angle bins
     - effective_area_3D_nominal uses a nominal coordinate system with FOV longitude and FOV latitude bins

--- pyirf/irf/effective_area.py
+++ pyirf/irf/effective_area.py
@@ -85,3 +85,69 @@ def effective_area_per_energy_and_fov(
     )
 
     return effective_area(hist_selected, hist_simulated, area)
+
+def effective_area_3D_polar(
+    selected_events, simulation_info, energy_bins, fov_offset_bins, fov_position_angle_bins
+):
+    """
+    Calculate effective area in bins of true energy, field of view offset, and field of view position angle.
+
+    Parameters
+    ----------
+    selected_events: astropy.table.QTable
+        DL2 events table, required columns for this function:
+        - `true_energy`
+        - `true_source_fov_offset`
+        - `true_source_fov_position_angle`
+    simulation_info: pyirf.simulations.SimulatedEventsInfo
+        The overall statistics of the simulated events
+    true_energy_bins: astropy.units.Quantity[energy]
+        The true energy bin edges in which to calculate effective area.
+    fov_offset_bins: astropy.units.Quantity[angle]
+        The field of view radial bin edges in which to calculate effective area.
+    fov_position_angle_bins: astropy.units.Quantity[radian]
+        The field of view azimuthal bin edges in which to calculate effective area.
+    """
+    area = np.pi * simulation_info.max_impact ** 2
+    
+    hist_simulated = simulation_info.calculate_n_showers_3D_radial(simulation_info, energy_bins, fov_offset_bins, fov_position_angle_bins)
+    
+    hist_selected,_ = np.histogramdd(
+            np.array([selected_events['true_energy'].to_value(u.TeV), selected_events['true_source_fov_offset'].to_value(u.deg), selected_events['true_source_fov_position_angle'].to_value(u.rad)]).T,
+            bins=(energy_bins.to_value(u.TeV), fov_offset_bins.to_value(u.deg), fov_position_angle_bins.to_value(u.rad)),
+        )
+    
+    return effective_area(hist_selected, hist_simulated, area)
+
+def effective_area_3D_nominal(
+    selected_events, simulation_info, energy_bins, fov_longitude_bins, fov_latitude_bins
+):
+    """
+    Calculate effective area in bins of true energy, field of view longitude, and field of view latitude.
+
+    Parameters
+    ----------
+    selected_events: astropy.table.QTable
+        DL2 events table, required columns for this function:
+        - `true_energy`
+        - `true_source_fov_lon`
+        - `true_source_fov_lat`
+    simulation_info: pyirf.simulations.SimulatedEventsInfo
+        The overall statistics of the simulated events
+    true_energy_bins: astropy.units.Quantity[energy]
+        The true energy bin edges in which to calculate effective area.
+    fov_longitude_bins: astropy.units.Quantity[angle]
+        The field of view longitude bin edges in which to calculate effective area.
+    fov_latitude_bins: astropy.units.Quantity[angle]
+        The field of view latitude bin edges in which to calculate effective area.
+    """
+    area = np.pi * simulation_info.max_impact ** 2
+    
+    hist_simulated = simulation_info.calculate_n_showers_3D_nominal(simulation_info, energy_bins, fov_longitude_bins, fov_latitude_bins)
+    
+    hist_selected, _ = np.histogramdd(
+        np.array([selected_events['true_energy'].to_value(u.TeV), selected_events['true_source_fov_lon'].value, selected_events['true_source_fov_lat'].value]).T,
+        bins=(energy_bins.to_value(u.TeV), fov_longitude_bins.to_value(u.deg), fov_latitude_bins.to_value(u.deg)),
+    )
+
+    return effective_area(hist_selected, hist_simulated, area)
