b5bce60d254fa225387eb17e687a77058c9ccb16
Luca Davide Di Bella <DibellaLuca@gmx.de>
2025-01-20 19:30:50 +0100

    Add missing imports

--- pyirf/io/__init__.py
+++ pyirf/io/__init__.py
@@ -2,6 +2,8 @@ from .eventdisplay import read_eventdisplay_fits
 from .gadf import (
     create_aeff2d_hdu,
     create_energy_dispersion_hdu,
+    create_energy_dispersion_asymmetric_polar_hdu,
+    create_energy_dispersion_asymmetric_lonlat_hdu,
     create_psf_table_hdu,
     create_rad_max_hdu,
     create_background_2d_hdu,
